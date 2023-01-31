5a83f819d1f4bb40c87ddad8d925b3ee9364c319
Paddy Wardle <paddy.wardle@yahoo.com>
2023-01-31 14:01:52 +0000

    common.h initial commit - putting common dependencies in one header file

---
+++ src/common.h
@@ -0,0 +1,9 @@
+#include <vector>
+#include <array>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <libconfig.h++>
+#include <eigen3/Eigen/Dense>
